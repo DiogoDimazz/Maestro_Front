321b2362e4b450709a6e0edf14839e1ed3a77781
Dimazz <diogo.compositor@gmail.com>
2023-07-03 15:42:50 -0400

    rascunho da parte visual do metronomo

---
+++ src/Components/SideDesigns/SideDesigns.js
@@ -0,0 +1,11 @@
+import './styles.css'
+import metronomeImage from '../../assets/Metronomo_2.png'
+
+export const SideDesigns = ({side}) => {
+
+    return (
+        <div className="side-background">
+            <img src={metronomeImage} alt='metronome-img' className='metronome-img' style={side}/>
+        </div>
+    )
+}
